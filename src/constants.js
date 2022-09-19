3e43eb9cfe29fc00b5a9875b3d91afe6a9792dab
Marco Antonio Dominguez <madmzr@gmail.com>
2022-09-19 04:06:56 -0400

    Adding setInto

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+// Took from lodash https://github.com/lodash/lodash/blob/ddfd9b11a0126db2302cb70ec9973b66baec0975/lodash.js#L146
+export const PATH_MATCHER =
+  /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g;
